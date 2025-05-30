85698a0160c8584f51c70747836fc332e63a15ee
attique <trsmarkhore@gmail.com>
2025-05-30 19:03:00 +0500

    third commit

---
+++ Images/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication9.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
